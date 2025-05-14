1ff44ffb4107c4b08e89a90a7a6b4ec2eee21cb1
Kawshikk-Shrii <kawshikkshriidatta@gmail.com>
2025-05-14 23:48:17 +0530

    Changed code

--- colorshow/src/App.js
+++ colorshow/src/App.js
@@ -3,62 +3,32 @@ import './App.css';
 import ColorName from './colorname';
 import { useState } from 'react';
 
-// function App() {
-//   const [color, setColor] = useState('');
-//   const stylecolor = {
-//     backgroundColor: color,
-//     height: '50px',
-//     width: '100px',
-//     border: '1px solid black',
-//     marginTop: '10px'
-//   };
-//   return (
-//     // <div className="App">
-//     //   <header className="App-header">
-//     //     <img src={logo} className="App-logo" alt="logo" />
-//     //     <p>
-//     //       Edit <code>src/App.js</code> and save to reload.
-//     //     </p>
-//     //     <a
-//     //       className="App-link"
-//     //       href="https://reactjs.org"
-//     //       target="_blank"
-//     //       rel="noopener noreferrer"
-//     //     >
-//     //       Learn React
-//     //     </a>
-//     //   </header>
-//     // </div>
-//     <>
-//       <ColorName style={stylecolor}
-//       color = { color }
-//       setColor = { setColor } />
-//     </>
-//   );
-// }
-
-// export default App;
-
 function App() {
   const [color, setColor] = useState('');
 
   const stylecolor = {
     backgroundColor: color,
+    type: 'text',
+    placeholder: 'Empty value',
     height: '50px',
     width: '100px',
-    border: '1px solid black',
-    marginTop: '10px'
+    border: '3px solid black',
+    marginTop: '10px',
+    boxshadow: '5px'
   };
 
   return (
     <div>
-      <ColorName color={color} setColor={setColor} />
-
-      <p style={{ color: color || 'black' }}>
+      <p style={{ color: color || 'black' }} >
         You typed: {color || 'No color yet'}
       </p>
 
       <div style={stylecolor} />
+
+      <ColorName 
+        color={color} 
+        setColor={setColor} 
+      />
     </div>
   );
 }
